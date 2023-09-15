6205badd385abd83c82aa8f64af65d2430d8ada1
edumaldo94 <maldonado19994@gmail.com>
2023-09-15 16:26:49 -0300

    fdg

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let guardar=[]
 const fs=require('fs')
 
 app.use(cors())
-app.use(express.static('../public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 app.use(express.json({type: "*/*"}))
 
