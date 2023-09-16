5d80aeb7d7166d52ceb7dd72b85ad125e42ea0f4
edumaldo94 <maldonado19994@gmail.com>
2023-09-16 16:11:25 -0300

    gffgf

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(cors())
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json({type: "*/*"}))
-app.listen(port, ()=>{console.log(`Servidor escuchando en el puerto http://localhost:${port}`)})
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${port}`)
+});
     
 
 
